2c775535e0f768a863c6a0c1af7172c13c2efa26
nikhil.dhumale29@gmail.com <nikhil.dhumale29@gmail.com>
2026-08-13 19:18:27 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-
- 
 from setuptools import find_packages, setup
 from typing import List
 
@@ -7,20 +5,16 @@ def get_requirements(file_path: str) -> List[str]:
     """
     Reads a requirements file and returns a list of dependencies.
     """
-    requirements = []
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements=[req. replace("\n","") for req in requirements]
-        if "-e ." in requirements:
-                requirements.remove("-e .")
-
+        requirements = [req.strip() for req in file_obj.readlines() if req.strip()]
+        requirements = [req for req in requirements if req != "-e ."]
     return requirements
 
 setup(
     name="mlProject",
     version="0.0.1",
-    author="k06@gmail.com",
+    author="Nikhil",
+    author_email="k06@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt"),
 )
- 
